a8a27580b598d5d426d511e523025b2f0fd4e3d5
Swapnil Joshi <61009651+hiswaps@users.noreply.github.com>
2020-03-13 09:19:46 +0200

    Update __init__.py

--- pytops/__init__.py
+++ pytops/__init__.py
@@ -1,1 +1,2 @@
-from pytops.Filename import Classname
+#from pytops.Filename import Classname #create classes and update this later
+from pytops.ops import Classname #create classes and update this later
